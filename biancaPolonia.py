f3462c3ff7fe25d8363517c9eda38867153958db
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-13 16:31:20 -0300

    Update biancaPolonia.py

--- biancaPolonia.py
+++ biancaPolonia.py
@@ -533,6 +533,8 @@ with col1:
                                             ax.plot(
                                                 [time_original_kinem[onsets[0]],time_original_kinem[onsets[0]]], [0,30], 'b-')
                                             ax.plot(
+                                                [time_original_kinem[offsets[0]],time_original_kinem[offsets[0]]], [0,30], 'b-')
+                                            ax.plot(
                                                 [time_original_kinem[onsets[1]],time_original_kinem[onsets[1]]], [0,30], 'b-')
                                             
                                             ax.plot([0, 0], [0, 30], 'r--')
@@ -606,6 +608,7 @@ with col1:
                                                 for idx in np.arange(4):
                                                     st.text(
                                                         f'Duração da volta {idx+1} = {sitting_time[idx] - time_original_kinem[peaks[idx]]}')
+
 
 
 
