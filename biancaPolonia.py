809813258c2148f2ef6e981f9ae41c1683b45b9c
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-13 15:50:25 -0300

    Update biancaPolonia.py

--- biancaPolonia.py
+++ biancaPolonia.py
@@ -515,7 +515,7 @@ with col1:
                                             ax.plot(
                                                 time_interpolated_gyro, ml_gyro, 'k-')
                                             ax.plot(
-                                                [time_original_kinem[onsets[0],[time_original_kinem[onsets[0]], [0,30], 'b-')
+                                                [time_original_kinem[onsets[0],time_original_kinem[onsets[0]], [0,30], 'b-')
                                             
                                             ax.plot([0, 0], [0, 30], 'r--')
                                             ax.set_xlabel("Tempo (s)")
@@ -588,6 +588,7 @@ with col1:
                                                 for idx in np.arange(4):
                                                     st.text(
                                                         f'Duração da volta {idx+1} = {sitting_time[idx] - time_original_kinem[peaks[idx]]}')
+
 
 
 
