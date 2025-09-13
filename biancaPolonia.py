1f529a866f9f016c8b0d6edba0df0cdaa462c16c
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-13 15:17:30 -0300

    Update biancaPolonia.py

--- biancaPolonia.py
+++ biancaPolonia.py
@@ -443,7 +443,7 @@ with col1:
                                                 figsize=(10, 4))
                                             ax.plot(
                                                 time_interpolated, ap_acc, 'k-')
-                                            ax.plot([0, 0], [0, 10], 'r--')
+                                            ax.plot([0, 0], [0, 30], 'r--')
                                             ax.set_xlabel("Tempo (s)")
                                             ax.set_ylabel("Aceleração AP")
                                             ax.set_ylim([0,10])
@@ -453,7 +453,7 @@ with col1:
                                             figsize=(10, 4))
                                             ax.plot(
                                                 time_interpolated, v_acc, 'k-')
-                                            ax.plot([0, 0], [0, 10], 'r--')
+                                            ax.plot([0, 0], [0, 30], 'r--')
                                             ax.set_xlabel("Tempo (s)")
                                             ax.set_ylabel("Aceleração V")
                                             ax.set_ylim([0,10])
@@ -499,6 +499,37 @@ with col1:
                                             ax.set_xlabel("Tempo (s)")
                                             ax.set_ylabel("Amplitude")
                                             st.pyplot(fig)
+
+                                            fig, ax = plt.subplots(
+                                            figsize=(10, 4))
+                                            ax.plot(
+                                                time_interpolated, ml_gyro, 'k-')
+                                            ax.plot([0, 0], [0, 30], 'r--')
+                                            ax.set_xlabel("Tempo (s)")
+                                            ax.set_ylabel("Velocidade angular ML")
+                                            ax.set_ylim([0,10])
+                                            st.pyplot(fig)
+
+                                            fig, ax = plt.subplots(
+                                                figsize=(10, 4))
+                                            ax.plot(
+                                                time_interpolated, ap_gyro, 'k-')
+                                            ax.plot([0, 0], [0, 30], 'r--')
+                                            ax.set_xlabel("Tempo (s)")
+                                            ax.set_ylabel("Velocidade angular AP")
+                                            ax.set_ylim([0,10])
+                                            st.pyplot(fig)
+
+                                            fig, ax = plt.subplots(
+                                            figsize=(10, 4))
+                                            ax.plot(
+                                                time_interpolated, v_gyro, 'k-')
+                                            ax.plot([0, 0], [0, 30], 'r--')
+                                            ax.set_xlabel("Tempo (s)")
+                                            ax.set_ylabel("Velocidade angular V")
+                                            ax.set_ylim([0,10])
+                                            st.pyplot(fig)
+                                            
                                             with col1:
                                                 st.text(
                                                     f'Número de ciclos = {num_ciclos}')
@@ -544,6 +575,7 @@ with col1:
                                                 for idx in np.arange(4):
                                                     st.text(
                                                         f'Duração da volta {idx+1} = {sitting_time[idx] - time_original_kinem[peaks[idx]]}')
+
 
 
 
