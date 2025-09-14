22d0e91652b58ad10544a7d2eba26372cd445492
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-14 12:15:36 -0300

    Update biancaPolonia.py

--- biancaPolonia.py
+++ biancaPolonia.py
@@ -1155,7 +1155,7 @@ with col1:
                                                 [momentos_picos2v_acc[0],momentos_picos2v_acc[0]], [0,30], 'k--')
                                                 ax.plot(
                                                 [momentos_picos2v_acc[1],momentos_picos2v_acc[1]], [0,30], 'k--')
-                                                ax.set_xlim([time_interpolated_gyro[start_gyro2-offset_gyro-100],time_interpolated_gyro[onset_gyro+start_gyro2]])
+                                                ax.set_xlim([time_interpolated_gyro[start_gyro2-offset_gyro-100],time_interpolated_gyro[onset_gyro+start_gyro2+100]])
                                                 ax.set_ylim([-0.5,5])
                                                 st.pyplot(fig)
                                                 
@@ -1294,6 +1294,7 @@ with col1:
                                                 for idx in np.arange(4):
                                                     st.text(
                                                         f'Duração da volta {idx+1} = {sitting_time[idx] - time_original_kinem[peaks[idx]]}')
+
 
 
 
