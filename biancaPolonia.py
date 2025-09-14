aa831c6d1d8c0263c506ef80601222afa4c75dd0
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-14 11:47:32 -0300

    Update biancaPolonia.py

--- biancaPolonia.py
+++ biancaPolonia.py
@@ -1096,6 +1096,37 @@ with col1:
                                             
                                                 ax.set_ylim([-0.5,5])
                                                 st.pyplot(fig)
+
+                                                fig, ax = plt.subplots(figsize=(10, 4))
+                                                ax.plot(time_original_kinem, disp_y, 'k-')
+
+                                                ax.plot(
+                                                [time_interpolated_gyro[onset_gyro+start_gyro],time_interpolated_gyro[onset_gyro+start_gyro]], [0,30], 'y--')
+                                                ax.plot(
+                                                [time_interpolated_gyro[start_gyro2-offset_gyro],time_interpolated_gyro[start_gyro2-offset_gyro]], [0,30], 'y--')
+                                                                                            
+                                                ax.plot(
+                                                [momentos_picos[0],momentos_picos[0]], [0,30], 'c--')
+                                                ax.plot(
+                                                [momentos_picos[1],momentos_picos[1]], [0,30], 'c--')
+
+                                                ax.plot(
+                                                [momentos_picosml[0],momentos_picosml[0]], [0,30], 'r--')
+                                                ax.plot(
+                                                [momentos_picosml[1],momentos_picosml[1]], [0,30], 'r--')
+
+                                                ax.plot(
+                                                [momentos_picosap_acc[0],momentos_picosap_acc[0]], [0,30], 'b--')
+                                                ax.plot(
+                                                [momentos_picosap_acc[1],momentos_picosap_acc[1]], [0,30], 'b--')
+
+                                                ax.plot(
+                                                [momentos_picosv_acc[0],momentos_picosv_acc[0]], [0,30], 'k--')
+                                                ax.plot(
+                                                [momentos_picosv_acc[1],momentos_picosv_acc[1]], [0,30], 'k--')
+                                                ax.set_xlim([time_interpolated_gyro[onset_gyro+start_gyro],time_interpolated_gyro[start_gyro2-offset_gyro]])
+                                                ax.set_ylim([-0.5,5])
+                                                st.pyplot(fig)
                                                 
                                                 st.text(
                                                     f'Número de ciclos = {num_ciclos}')
@@ -1232,6 +1263,7 @@ with col1:
                                                 for idx in np.arange(4):
                                                     st.text(
                                                         f'Duração da volta {idx+1} = {sitting_time[idx] - time_original_kinem[peaks[idx]]}')
+
 
 
 
